2849d34834e14b4fe1906e7164ee4aaa523f0450
Ana França <ana.franca@pier.digital>
2020-06-04 10:05:46 -0300

    Add GET /items endpoint

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,10 +1,19 @@
 import express from "express";
+import knex from "./database/connection";
 
 const routes = express.Router();
 
-routes.get("/users", (request, response) => {
-  console.log("Listagem de usuários");
-  response.send("Hello World");
+routes.get("/items", async (request, response) => {
+  const items = await knex("items").select("*");
+
+  const serializedItems = items.map(item => {
+    return {
+      title: item.title,
+      image_url: `http://localhost:3333/uploads/${item.image}`,
+    };
+  });
+
+  response.json(serializedItems);
 });
 
 export default routes;
