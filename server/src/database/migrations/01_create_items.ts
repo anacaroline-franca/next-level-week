cbae3ffcb5ab990faf0bf60365f88e9a8dbf7cb3
Ana França <ana.franca@pier.digital>
2020-06-04 09:19:16 -0300

    Adjust on DB and add seed command

--- server/src/database/migrations/01_create_items.ts
+++ server/src/database/migrations/01_create_items.ts
@@ -3,7 +3,7 @@ import Knex from "knex";
 export async function up(knex: Knex) {
   return knex.schema.createTable("items", table => {
     table.increments("id").primary();
-    table.string("images").notNullable();
+    table.string("image").notNullable();
     table.string("title").notNullable();
   });
 };
