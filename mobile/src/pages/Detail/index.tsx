8a9c096b195c9e6234050cb73ad415d8e028575d
Ana França <ana.franca@pier.digital>
2020-06-07 17:47:36 -0300

    Add static Detail page

--- mobile/src/pages/Detail/index.tsx
+++ mobile/src/pages/Detail/index.tsx
@@ -1,11 +1,47 @@
 import React from "react";
-import { View, ImageBackground, Image, StyleSheet, Text } from "react-native";
+import { View, Image, StyleSheet, Text, TouchableOpacity, SafeAreaView } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { RectButton } from "react-native-gesture-handler";
+import { Feather as Icon, FontAwesome } from "@expo/vector-icons";
+import Constants from "expo-constants";
 
 const Detail = () => {
-  return (
-    <View>
+  const navigation = useNavigation();
 
-    </View>
+  function goBack() {
+    navigation.goBack();
+  }
+
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <TouchableOpacity onPress={goBack}>
+          <Icon name="arrow-left" size={20} color="#34cb79" />
+        </TouchableOpacity>
+
+        <Image style={styles.pointImage} source={{ uri: "https://images.unsplash.com/photo-1503596476-1c12a8ba09a9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60" }} />
+
+        <Text style={styles.pointName}>Mercado Example</Text>
+        <Text style={styles.pointItems}>Lampadas, oleo de cozinha</Text>
+
+        <View style={styles.address}>
+          <Text style={styles.addressTitle}>Endereço</Text>
+          <Text style={styles.addressContent}>São Paulo, SP</Text>
+        </View>
+      </View>
+
+
+      <View style={styles.footer}>
+        <RectButton style={styles.button} onPress={() => { }}>
+          <FontAwesome name="whatsapp" size={20} color="#FFF" />
+          <Text style={styles.buttonText}>Whatsapp</Text>
+        </RectButton>
+        <RectButton style={styles.button} onPress={() => { }}>
+          <Icon name="mail" size={20} color="#FFF" />
+          <Text style={styles.buttonText}>Email</Text>
+        </RectButton>
+      </View>
+    </SafeAreaView>
   );
 };
 
@@ -13,7 +49,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 32,
-    paddingTop: 20,
+    paddingTop: 20 + Constants.statusBarHeight,
   },
 
   pointImage: {
